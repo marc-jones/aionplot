673a9dee4ef1872456e9d59b69fe67ce161c5599
Marc Jones <m4rcj0n35@gmail.com>
2018-07-31 16:58:02 +0100

    Moved selection box setup to separate file

--- webapp_image/app/templates/SearchAndPlot/EverythingElse.js
+++ webapp_image/app/templates/SearchAndPlot/EverythingElse.js
@@ -1,35 +1,3 @@
-// Sets up the search box
-$(document).ready(function(){
-    $('#search-box').selectize({
-        valueField: 'name',
-        labelField: 'name',
-        searchField: ['name', 'nicknames'],
-        plugins: ['remove_button'],
-        options: [],
-        create: false,
-        load: function(query, callback) {
-            if (!query.length) return(callback());
-            $.ajax({
-                url: "{{ url_for('autocomplete') }}" + "?term=" + query ,
-                error: function() {callback();},
-                success: function(res) {callback(res.json_list);}
-            });
-        },
-        render: {
-            option: function(item, escape) {
-                return(
-    '<div>' +
-        '<span class="title">' +
-            '<span class="name">' + escape(item.name) + '</span>' +
-            '<br>' +
-            '<span class="nicknames">' + escape(item.nicknames) + '</span>' +
-        '</span>' +
-    '</div>');
-            }
-        }
-    });
-})
-
 // Sets up the Download buttons
 $(document).ready(function(){
     formatTitle();
