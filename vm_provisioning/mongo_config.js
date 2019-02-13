bfdd2fceea615c984f682b7e57ef4273d35bee65
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-02-13 13:46:43 +0000

    Use vars and templating for mongo user names

--- vm_provisioning/mongo_config.js
+++ vm_provisioning/mongo_config.js
@@ -1,12 +1,12 @@
 db = db.getSiblingDB('time_series');
 db.test.insert({'test': 'test'});
 db.createUser({
-  user: 'webapp',
+  user: '{{ mongo_webapp_username }}',
   pwd: '{{ mongo_webapp_password }}',
   roles: ['read']
 });
 db.createUser({
-  user: 'dbadmin',
+  user: '{{ mongo_dbadmin_username }}',
   pwd: '{{ mongo_dbadmin_password }}',
   roles: ['readWrite', 'dbAdmin']
 });
